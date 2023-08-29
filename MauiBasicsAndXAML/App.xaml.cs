81a8eb61b22b132b990c61008f55c590f1b75f64
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2023-08-29 10:36:00 -0500

    Add project files.

---
+++ MauiBasicsAndXAML/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiBasicsAndXAML;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiBasicsAndXAML/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiBasicsAndXAML;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
