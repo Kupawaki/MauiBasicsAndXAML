ac050099f8f369bcc36e89b2cee359c9a6540277
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2023-08-30 14:14:55 -0500

    Action Page Complete

--- MauiBasicsAndXAML/ActionPage.xaml.cs
+++ MauiBasicsAndXAML/ActionPage.xaml.cs
@@ -6,4 +6,29 @@ public partial class ActionPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+		DisplayAlert("This is an alert!", "Things are good", "Okay");
+    }
+
+    private void OnImageButtonClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("You have clicked the image button", "", "okay");
+    }
+
+    private void SearchBar_SearchButtonPressed(object sender, EventArgs e)
+    {
+        DisplayAlert("You Did A Thing!", $"You searched for {SearchBar.Text}", "Okay");
+    }
+
+    private void RightThing_Invoked(object sender, EventArgs e)
+    {
+        DisplayAlert("You Did A Swipe!", "Right Thing", "Okay");
+    }
+
+    private void LeftThing_Invoked(object sender, EventArgs e)
+    {
+        DisplayAlert("You Did A Swipe!", "Left Thing", "Okay");
+    }
 }
